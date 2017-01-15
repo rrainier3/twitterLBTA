0f8e53cf007f4c71a19dd2a2cd07b52144b9c466
Ray Rainier <rayrainier@yahoo.com>
2017-01-15 12:26:33 -0600

    added class: WordCell()

--- twitterLBTA/HomeController.swift
+++ twitterLBTA/HomeController.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
+class WordCell: UICollectionViewCell {
+    
+    // this gets called when a cell is dequeued
+    override init(frame: CGRect) {
+    	super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
 	let cellId = "cellId"
@@ -15,8 +29,8 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .green
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.backgroundColor = .white
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
         
     }
     
@@ -27,8 +41,6 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        
-        cell.backgroundColor = .blue
         
         return cell
     }
