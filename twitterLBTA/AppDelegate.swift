c04b4b2abb215a6a3e88e8ea7301c97d6fae2d5b
Ray Rainier <rayrainier@yahoo.com>
2017-01-15 11:55:27 -0600

    Created homeController: UICollectionView

--- twitterLBTA/AppDelegate.swift
+++ twitterLBTA/AppDelegate.swift
@@ -21,8 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        //window?.rootViewController = UINavigationController(rootViewController: ViewController())
         
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.rootViewController = UINavigationController(rootViewController: homeController)
         
         return true
     }
---
+++ twitterLBTA/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  twitterLBTA
+//
+//  Created by RayRainier on 1/15/17.
+//  Copyright © 2017 RadiuSense. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+        
+    }
+}
