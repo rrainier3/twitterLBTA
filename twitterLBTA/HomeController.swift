8cf0cacb9461d3db4184dbadc98f1de5a09ea677
Ray Rainier <rayrainier@yahoo.com>
2017-01-15 12:17:14 -0600

    Home Controller updates

--- twitterLBTA/HomeController.swift
+++ twitterLBTA/HomeController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+	let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .green
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
 }
