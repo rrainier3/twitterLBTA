bfb279529833e63bf32e3ea913841af786e0a849
Ray Rainier <rayrainier@yahoo.com>
2017-01-15 17:00:57 -0600

    MVC: Cells.swift, HomeDatasource.swift & HomeDatasourceController.swift

--- twitterLBTA/HomeDataSourceController.swift
+++ twitterLBTA/HomeDataSourceController.swift
@@ -8,45 +8,6 @@
 
 import LBTAComponents
 
-class UserCell: DatasourceCell {
-
-	override var datasourceItem: Any? {
-        didSet {
-            nameLabel.text = datasourceItem as? String
-        }
-    }
-    
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "TEST TEST TEST"
-        return label
-    }()
-    
-    override func setupViews() {
-        super.setupViews()
-        backgroundColor = .yellow
-        
-        addSubview(nameLabel)
-        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
-    }
-}
-
-class HomeDataSource: Datasource {
-    let words = ["User1", "User2", "User3"]
-    
-    override func cellClasses() -> [DatasourceCell.Type] {
-        return [UserCell.self]
-    }
-    
-    override func item(_ indexPath: IndexPath) -> Any? {
-        return words[indexPath.item]
-    }
-    
-    override func numberOfItems(_ section: Int) -> Int {
-        return words.count
-    }
-}
-
 class HomeDataSourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,5 +15,13 @@ class HomeDataSourceController: DatasourceController {
         let homeDataSource = HomeDataSource()
         self.datasource = homeDataSource
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
     }
 }
---
+++ twitterLBTA/HomeDatasource.swift
@@ -0,0 +1,33 @@
+//
+//  HomeDatasource.swift
+//  twitterLBTA
+//
+//  Created by RayRainier on 1/15/17.
+//  Copyright © 2017 RadiuSense. All rights reserved.
+//
+
+import LBTAComponents
+
+class HomeDataSource: Datasource {
+    let words = ["User1", "User2", "User3"]
+    
+    override func headerClasses() -> [DatasourceCell.Type]? {
+        return [UserHeader.self]
+    }
+    
+    override func footerClasses() -> [DatasourceCell.Type]? {
+        return [UserFooter.self]
+    }
+    
+    override func cellClasses() -> [DatasourceCell.Type] {
+        return [UserCell.self]
+    }
+    
+    override func item(_ indexPath: IndexPath) -> Any? {
+        return words[indexPath.item]
+    }
+    
+    override func numberOfItems(_ section: Int) -> Int {
+        return words.count
+    }
+}
