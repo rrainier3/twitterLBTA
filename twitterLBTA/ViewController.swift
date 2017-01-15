2b8e49659fe4cff795bf7748994db329fc5a4041
Ray Rainier <rayrainier@yahoo.com>
2017-01-15 10:54:51 -0600

    Initial Commit

---
+++ twitterLBTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterLBTA
+//
+//  Created by RayRainier on 1/15/17.
+//  Copyright © 2017 RadiuSense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
