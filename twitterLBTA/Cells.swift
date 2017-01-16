181eff03288cdbd5d98e05e8022fd1839fa180a7
Ray Rainier <rayrainier@yahoo.com>
2017-01-16 09:46:24 -0600

    Added followButton: UIButton

--- twitterLBTA/Cells.swift
+++ twitterLBTA/Cells.swift
@@ -52,6 +52,18 @@ class UserCell: DatasourceCell {
         return label
     }()
     
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
+    }()
+    
     override func setupViews() {
         super.setupViews()
         //backgroundColor = .yellow
@@ -60,11 +72,18 @@ class UserCell: DatasourceCell {
         addSubview(profileImageView)
         addSubview(nameLabel)
         addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
         
         profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
         
-        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
         
         usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
+    
 }
