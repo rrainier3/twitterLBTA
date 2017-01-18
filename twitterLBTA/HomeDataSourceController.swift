7f7dddc714ff0551363e94779f87c8053794589d
Ray Rainier <rayrainier@yahoo.com>
2017-01-17 21:21:12 -0600

    implement Custom Navigation Bar

--- twitterLBTA/HomeDataSourceController.swift
+++ twitterLBTA/HomeDataSourceController.swift
@@ -20,7 +20,13 @@ class HomeDataSourceController: DatasourceController {
     }
     
     private func setupNavigationBarItems() {
-    	
+        
+        setupLeftNavItem()
+        setupRightNavItems()
+        setupRemainingNavItems()        
+    }
+    
+    private func setupRemainingNavItems() {
         // display twitter_icon in the middle of navigation bar
         let titleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
         titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
@@ -28,18 +34,29 @@ class HomeDataSourceController: DatasourceController {
         
         navigationItem.titleView = titleImageView
         
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    private func setupLeftNavItem() {
         // display follow button on left of navigation bar
         let followButton = UIButton(type: .system)
         followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
         followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
-        
+    }
+    private func setupRightNavItems() {
         // display search button on right of navigation bar
         let searchButton = UIButton(type: .system)
         searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
         searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: searchButton)
         
+        // display compose button on right of navigation bar
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton),UIBarButtonItem(customView: searchButton)]
     }
     
     // collapse line spacing to remove gap
