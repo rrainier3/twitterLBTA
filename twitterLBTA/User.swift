ee7f0d71df0ef8717e26ebe2b8edb52cd7fb9d20
Ray Rainier <rayrainier@yahoo.com>
2017-03-01 10:27:39 -0600

    removed profileImage property since it is no longer needed.  Images moving forward are downloaded from the REST source

--- twitterLBTA/User.swift
+++ twitterLBTA/User.swift
@@ -13,16 +13,15 @@ struct User {
     let name: String
     let username: String
     let bioText: String
-    let profileImage: UIImage
     let profileImageUrl: String
     
     init(json: JSON) {
+    
         self.name = json["name"].stringValue
         self.username = json["username"].stringValue
         self.bioText = json["bio"].stringValue
         self.profileImageUrl = json["profileImageUrl"].stringValue
-        
-        self.profileImage = UIImage()
+
     }
 }
 
--- twitterLBTA/UserCell.swift
+++ twitterLBTA/UserCell.swift
@@ -19,7 +19,6 @@ class UserCell: DatasourceCell {
             nameLabel.text = user.name
             usernameLabel.text = user.username
             bioTextView.text = user.bioText
-            profileImageView.image = user.profileImage
             
             profileImageView.loadImage(urlString: user.profileImageUrl)
         }
